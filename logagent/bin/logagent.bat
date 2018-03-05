cbeb1320656db020933f46fed0d3f20d429a91b3
xhanyifeng <xhanyifeng@gmail.com>
2018-03-05 18:23:29 +0800

    update sensor-logagent version 20180125

--- logagent/bin/logagent.bat
+++ logagent/bin/logagent.bat
@@ -22,5 +22,5 @@ set "LOG_AGENT_MAIN_CLASS=com.sensorsdata.analytics.tools.logagent.LogAgentMain"
 set CMD_LINE_ARGS=%*
 
 set _EXECJAVA=java
-%_EXECJAVA% -server -classpath "%LOG_AGENT_LIB%" -Dsa.root.logger=DEBUG,DRFA,console -Dsa.log.dir="%LOG_AGENT_LOG_DIR%" -Dsa.log.file=logagent.log -Dfile.encoding=UTF-8 -Xms256m -Xmx256m -XX:+UseParallelGC -XX:ParallelGCThreads=4 -XX:+UseParallelOldGC -XX:YoungGenerationSizeIncrement=20 -XX:TenuredGenerationSizeIncrement=20 %LOG_AGENT_MAIN_CLASS% %CMD_LINE_ARGS%
+%_EXECJAVA% -server -classpath "%LOG_AGENT_LIB%" -Dsa.root.logger=DEBUG,DRFA,console -Dsa.log.file="%LOG_AGENT_LOG_DIR%\logagent.log" -Dfile.encoding=UTF-8 -Xms256m -Xmx256m -XX:+UseParallelGC -XX:ParallelGCThreads=4 -XX:+UseParallelOldGC -XX:YoungGenerationSizeIncrement=20 -XX:TenuredGenerationSizeIncrement=20 %LOG_AGENT_MAIN_CLASS% %CMD_LINE_ARGS%
 pause
