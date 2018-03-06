ef29b2a6187de36936c45bd5ab6d813c045b5c96
xhanyifeng <xhanyifeng@gmail.com>
2018-03-06 18:08:21 +0800

    update logagent version,add some env

--- init.sh
+++ init.sh
@@ -3,10 +3,12 @@ set -e
 if [ -n "$SENSORS_HOST" ]; then
 	if [ -z "$SENSORS_PORT" ]; then
         sed -i "s/\$SENSORS_HOST/$SENSORS_HOST/g" /etc/logagent.conf
-        sed -i "s/\$SENSORS_PORT/8006/g" /etc/logagent.conf
+        sed -i "s/\$SENSORS_PORT/8106/g" /etc/logagent.conf
+        sed -i "s/\$LOGAGENT_ID//g" /etc/logagent.conf
     else
     	sed -i "s/\$SENSORS_HOST/$SENSORS_HOST/g" /etc/logagent.conf
     	sed -i "s/\$SENSORS_PORT/$SENSORS_PORT/g" /etc/logagent.conf
+        sed -i "s/\$LOGAGENT_ID/$LOGAGENT_ID/g" /etc/logagent.conf
     fi
 fi
 
